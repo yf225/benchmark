0f611ac07592fbfe0f2ee76b0555c9cfb505bf0f
Will Feng <willfeng@fb.com>
2018-11-01 17:07:25 -0400

    Fix build_deps.sh

--- timing/cpp2/build_deps.sh
+++ timing/cpp2/build_deps.sh
@@ -10,7 +10,7 @@ pushd build
 mkdir gbenchmark
 mkdir gbenchmark_install
 pushd gbenchmark
-cmake -G "Unix Makefiles" ../../../../third_party/benchmark/ -DBENCHMARK_DOWNLOAD_DEPENDENCIES=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../gbenchmark_install
+cmake -G "Unix Makefiles" ../../../../../third_party/benchmark/ -DBENCHMARK_DOWNLOAD_DEPENDENCIES=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../gbenchmark_install
 make -j $(nproc)
 make install
 popd
