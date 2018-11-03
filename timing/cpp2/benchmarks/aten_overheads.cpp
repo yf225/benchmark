949c2cce909c5a9f57a2fd05f761995d16a2e5a0
Will Feng <willfeng@fb.com>
2018-11-02 23:07:35 -0400

    add wipe_cache()

--- timing/cpp2/benchmarks/aten_overheads.cpp
+++ timing/cpp2/benchmarks/aten_overheads.cpp
@@ -5,6 +5,41 @@
 // TODO: is -O0 a better idea for preventing loading cached value? How to let GCC evaluate the function every time?
 // TODO: read https://gcc.gnu.org/onlinedocs/gcc/Optimize-Options.html
 
+static uint32_t wipe_cache() {
+  static uint32_t* wipe_buffer = nullptr;
+  static size_t wipe_size = 0;
+
+  if (wipe_buffer == nullptr) {
+    /*
+      On g3.8xlarge:
+      L1d cache:             32K
+      L1i cache:             32K
+      L2 cache:              256K
+      L3 cache:              46080K
+      Data cache total:      46368K
+    */
+    wipe_size = 32 * 1024;  // 32K
+    wipe_buffer = static_cast<uint32_t*>(malloc(wipe_size));
+    AT_ASSERT(wipe_buffer != nullptr);
+  }
+  uint32_t hash = 0;
+  for (uint32_t i = 0; i * sizeof(uint32_t) < wipe_size; i += 8) {
+    hash ^= wipe_buffer[i];
+    wipe_buffer[i] = hash;
+  }
+  /* Make sure compiler doesn't optimize the loop away */
+  return hash;
+}
+
+static void BM_WipeCache(benchmark::State& state) {
+  auto options = at::TensorOptions(at::kCPU);
+
+  for (auto _ : state) {
+    benchmark::DoNotOptimize(wipe_cache());
+  }
+}
+BENCHMARK(BM_WipeCache);
+
 static void BM_TensorDim(benchmark::State& state) {
   auto options = at::TensorOptions(at::kCPU);
 
@@ -13,6 +48,7 @@ static void BM_TensorDim(benchmark::State& state) {
   int64_t res = 0;
 
   for (auto _ : state) {
+    benchmark::DoNotOptimize(wipe_cache());
     benchmark::DoNotOptimize(res = tmp.dim());
   }
   std::ostream cnull(0);
